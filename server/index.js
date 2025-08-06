0a1de055fead32d1e67752919a183e8ed2cde106
Rushikesh Padaval <rushikeshpadaval@outlook.com>
2025-08-06 13:00:36 +0530

    app config

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/posts');
 const userRoutes = require('./routes/users');
 
+//app config
 const app = express();
+const port = process.env.PORT || 5000; 
+
+
+//middleware
 app.use(cors());
 app.use(express.json());
 
