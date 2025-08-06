d152caf0ee4a9c6433a8561a5193076b1f1d6ba9
Rushikesh Padaval <rushikeshpadaval@outlook.com>
2025-08-06 14:38:13 +0530

    minor changes

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: process.env.NODE_ENV === 'production' 
-    ? 'https://community-app-server.onrender.com'
+    ? 'https://community-app-server.onrender.com/api'
     : 'http://localhost:5000/api'
 });
 export default API;
